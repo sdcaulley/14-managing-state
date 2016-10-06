9cdc365936df98476f13225f9abd39f88889714a
sdcaulley <stephanie.caulley@gmail.com>
2016-10-06 14:01:06 -0700

    added comments to articleController

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -21,6 +21,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  //This function is called when a author is selected from the dropdown.  It calles Article.findWhere.  Article.findWhere is executing a sql SELECT useing the author name as the value of the WHERE clause, and passes authorData as the callback. authorData is saving the author name as a context variable. It is part of the middleware chain for /author/:authorName.
   articlesController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
@@ -33,6 +34,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  //This function does the same thing as loadByAuthor, only for the category selector. It is part of the middleware for /category/:categoryName.
   articlesController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
       ctx.articles = articlesInCategory;
@@ -43,6 +45,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  //This function is called on the root path.  It looks to see if the Article.all has items.  If yes then it saves the array as a ctx parameter and calls articlesController.index.  If no, then calls Article.fetchAll and passes it articleData as its callback. Article.data saves the articles as context parameters. And then calls articlesController.index. This is middleware for the root url.
   articlesController.loadAll = function(ctx, next) {
     var articleData = function(allArticles) {
       ctx.articles = Article.all;
